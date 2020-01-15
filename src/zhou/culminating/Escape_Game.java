d510ae5a892e72bd7a9298c09176f9a569bade9c
Shuu <45219546+Shizuekana@users.noreply.github.com>
2020-01-15 13:26:10 -0500

    push

--- src/zhou/culminating/Escape_Game.java
+++ src/zhou/culminating/Escape_Game.java
@@ -7,23 +7,14 @@ import java.io.InputStream;
 
 import hsafx.Console;
 import hsafx.ConsoleView;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 public class Escape_Game extends ConsoleView{
 
 	Image[] backgrounds = new Image[20];
+	Image[] items = new Image[20];
 	boolean[] challenges = new boolean[10];
-
-	BorderPane borderPane = new BorderPane();
-	HBox hbox =  new HBox();
-	//borderPane.setTop(hbox);
-
-	Scene s = new Scene(borderPane, 800, 600);
-
 	Console c;
 
 	// *****************************************
@@ -33,38 +24,80 @@ public class Escape_Game extends ConsoleView{
 
 	@Override
 	public void run() {
-		int screen = 0;
+		int screen = 1;
+		int box = 2;
 		c = new Console (800, 600, 12,"Game", this);
 
-
-		c.println("hey");
-
-		//drawScreen(screen);
-		//mouse();
-	}
-
-	public void drawScreen(int screen) {
-
-		if (screen == 0) {
-			c.drawImage(backgrounds[screen], 0, 0, 800, 600);
-
-			// textbox at bottom.
-			c.strokeRect(50, 300, 500 , 250);
-			c.setFill(Color.ANTIQUEWHITE);
-			c.fillRect(49, 299, 499, 249);
-
+		try {
+			loadImages();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 
+		c.autoRefreshOff();
+		drawScreen(screen, box);
+
+		c.setOnMouseMoved(e->{
+			//	System.out.println("Here");
+			if (e.getX() > 50 && e.getX() < 100 && e.getY() > 50 && e.getY() < 100) {
+				c.setStroke(Color.BLUE);
+				c.strokeRect(50, 50, 50, 50);
+			}
+		});
+
+		c.setOnMouseClicked(e->{
+			int boxy = 1;
+			if (e.getX() > 50 && e.getX() < 550 && e.getY() > 400 && e.getY() < 750) {
+				drawScreen(screen, boxy++);
+			}
+		});
+
+		c.refresh();
+		c.sleep(30);
+
+
+	}
+
+	public void drawScreen(int screen, int box) {
+
 		if (screen == 1) {
-			c.drawImage(backgrounds[screen], 0, 0, 800, 600);
-			c.strokeRect(50, 300, 500 , 250);
-			c.setFill(Color.ANTIQUEWHITE);
-			c.fillRect(49, 299, 499, 249);
+
+			c.drawImage(backgrounds[1], 0, 0, 800, 600);
+
+			// c.strokeText("Hello", 100, 200);
+			c.strokeRect(50, 400, 700 , 150); // x-co, y-co, width, height
+			c.setFill(Color.LIGHTSTEELBLUE);
+			c.fillRect(50, 400, 700, 150);
+
+			if (box == 1) {
+				c.setFont("Times New Roman", 16);
+				c.setFill(Color.BLACK);
+				c.fillText("You wake up in a white room.", 60, 430);
+			}
+
+			if (box == 2) {
+				c.setFont("Times New Roman", 16);
+				c.setFill(Color.BLACK);
+				c.fillText("Wow! What do you think that is over there? \n... \n"
+						+ "It appears to be a safe. \n! \n There is a note next to it.", 60, 430);
+				c.drawImage(items[1], 200, 100, 400, 250);
+			}
 		}
 
 		if (screen == 2) {
+			c.drawImage(backgrounds[screen], 0, 0);
+			c.strokeRect(50, 300, 700 , 250);
+			c.setFill(Color.ANTIQUEWHITE);
+			c.fillRect(50, 300, 700, 250);
 
 		}
+
+		if (screen == 3) {
+
+		}
+
+		//		c.setFill(Color.GREENYELLOW);
+		//		c.fillRect(50, 50, 50, 50);
 	}
 
 	public boolean checkRoom(int room) {
@@ -85,40 +118,12 @@ public class Escape_Game extends ConsoleView{
 
 	public void loadImages() throws FileNotFoundException {
 
-		InputStream zero = new FileInputStream(new File("scr/zhou/culminating/images/white_room.jpg"));
-		backgrounds[0] = new Image(zero, 800, 600, false, true);
-		// InputStream one = new FileInputStream(new File(""));
-		// backgrounds[1] = new Image(one, 800, 600, false, true);
+		InputStream oneBackground = new FileInputStream(new File("src/zhou/culminating/images/white_room.jpg"));
+		backgrounds[1] = new Image(oneBackground, 1000, 800, true, true);
+
+		InputStream oneItem = new FileInputStream(new File("src/zhou/culminating/images/safe.jpg"));
+		items[1] = new Image(oneItem, 800, 600, false, true);
 
 	}
 
-	public void mouse() {
-
-		if (c.getMouseDX() != 0 || c.getMouseDY() != 0) {
-			double left = c.getMouseX() - c.getMouseDX();
-			double right = c.getMouseX();
-			double top = c.getMouseY() - c.getMouseDY();
-			double bottom = c.getMouseY();
-			if (left > right) {
-				double temp = left;
-				left = right;
-				right = temp;
-			}
-			if (top > bottom) {
-				double temp = top;
-				top = bottom;
-				bottom = temp;
-			}
-			c.strokeRect(left, top, right - left + 1, bottom - top + 1);
-		}
-		if (c.getMouseButton(0)) {
-			c.setFill(Color.DARKRED);
-			c.fillStar(c.getMouseX() - 10, c.getMouseY() - 10, 21, 21);
-		} else {
-			c.setStroke(Color.DARKRED);
-			c.strokeStar(c.getMouseX() - 10, c.getMouseY() - 10, 21, 21);
-		}
-		c.refresh();
-		c.sleep(30);
-	}
 }
